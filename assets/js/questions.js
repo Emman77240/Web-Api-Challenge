dfdeee520930cd0748d0cb6791ec5ab5623e3824
Emman77240 <asiegbuemmanuelekee@gmail.com>
2023-01-18 01:40:20 +0000

    Added test questions as an array in questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,84 @@
 
-
-//"Is JavaScript a case-sensitive language?"
-//"Yes", "No", "Maybe", "I have no idea." 
+// Test Quesions
+let questions = [
+    {
+        question: "Javascript is an ______ language?",
+        answers: [
+            "Object-Oriented",
+            "Object-Based", 
+            "Procedural", 
+            "None of the above"
+        ],
+        correctAnswer: "Object-Oriented"
+    },
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        answers: ["var", "let", "Both A and B", "None of the above"],
+        correctAnswer: "Both A and B"
+    },
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        answers: [
+            "getElementbyId()", 
+            "getElementsByClassName()", 
+            "Both A and B", 
+            "None of the above"
+        ],
+        correctAnswer: "Both A and B"
+    },
+    {
+        question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        answers: [
+            "Throws an error", 
+            "Ignores the statements", 
+            "Gives a warning", 
+            "None of the above"
+        ],
+        correctAnswer: "Ignores the statements"
+    },
+    {
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        answers: [
+            "document.write()", 
+            "console.log()", 
+            "window.alert()", 
+            "All of the above"
+        ],
+        correctAnswer: "All of the above"
+    },
+    {
+        question: "How can a data type be declared to be a constant?",
+        answers: ["const", "var", "let", "constant"],
+        correctAnswer: "const"
+    },
+    {
+        question: "When a switch statement matches an expression with the given labels, how is the comparison done?",
+        answers: [
+            "Both the datatype and the result of the expression are compared", 
+            "Only the datatype of the expression is compared", 
+            "Only the value of the expression is compared", 
+            "None of the above"
+        ],
+        correctAnswer: "Both the datatype and the result of the expression are compared"
+    },
+    {
+        question: "What keyword is used to check whether a given property is valid or not?",
+        answers: ["in", "is in", "exists", "lies"],
+        correctAnswer: "in"
+    },
+    {
+        question: "What is the use of the <noscript> tag in Javascript?",
+        answers: [
+            "The contents are displayed by non-JS-based browsers",
+            "Clears all the cookies and cache", 
+            "Both A and B", 
+            "None of the above"
+        ],
+        correctAnswer: "The contents are displayed by non-JS-based browsers"
+    },
+    {
+        question: "When an operaor's value is NULL, the typeof returned by the unary operator is:?", 
+        answers: ["Boolean", "Undefined", "Object", "Integer"],
+        correctAnswer: "Object"
+    }
+]
